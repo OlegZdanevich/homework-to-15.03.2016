4f64ae97a44731d2e3123b1e985a48501c3ddb6c
Oleg Zdanevic <ya.zdanevich@yandex.ru>
2016-03-16 10:42:54 +0300

    Update Source.cpp

--- Homework16.03.2016/Source.cpp
+++ Homework16.03.2016/Source.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-#include <string.h>
+ #include <string.h>
 
 using namespace std;
 
 char** AllocArrayOfWords(int);
 void CharSwap(char* &, char* &);
-void Display(int*, char**,int n);
-char** ObtainWords(char* , int &);
+void Display(int*, char**, int n);
+char** ObtainWords(char*, int &);
 int* SearchAndCalculateRightWords(char** str, int k, char **words, int n);
-void ShakerSort(int *a, int n,char**words);
+void ShakerSort(int *a, int n, char**words);
 void Swap(int &x, int &y);
-
+void Swap(char* &x, char* &y);
 
 const char* alphabet = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
 
@@ -18,15 +18,15 @@ const char* alphabet = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
 
 
 int main()
-{
+ {
 	
-	while (true)
-	{
+		while (true)
+		 {
 		cout.width(50);
 		cout << endl << "Welcome to the programm!";
 		cout << endl << endl << "*******************************************************************************" << endl << endl;
 		
-		char *str = new char[256];
+			char *str = new char[256];
 		cout << "Enter the string: ";
 		cin.getline(str, 255, '\n');
 		cout << "How many words do you want to search? ";
@@ -38,8 +38,8 @@ int main()
 		int m = 0;
 		char **WordsString = ObtainWords(str, m);
 		int *a = SearchAndCalculateRightWords(WordsString, m, words, n);
-		ShakerSort(a, n,words);
-		Display(a, words,n);
+		ShakerSort(a, n, words);
+		Display(a, words, n);
 		system("pause");
 		system("cls");
 		char s;
@@ -51,106 +51,110 @@ int main()
 		system("cls");
 		if (s == 'y' || s == 'Y') continue;
 		break;
-
+		
+			}
+	
+		return 0;
 	}
-
-	return 0;
-}
 
 int* SearchAndCalculateRightWords(char** str, int k, char **words, int n)
-{
+ {
 	int *a = new int[n];
 	for (int i = 0; i < n; i++)
-	{
+		 {
 		a[i] = 0;
 		for (int j = 0; j < k; j++)
-		{
+			 {
 			if (strcoll(str[j], words[i]) == 0)
-			{
+				 {
 				a[i]++;
+				}
 			}
 		}
-	}
 	return a;
-}
+	}
 
 char** AllocArrayOfWords(int n)
-{
+ {
 	char**arr = new char*[n];
 	getchar();
 	for (int i = 0; i < n; i++)
-	{
+		 {
 		cout << "Enter " << i + 1 << " word: ";
 		arr[i] = new char[256];
 		cin.getline(arr[i], 255, '\n');
-	}
+		}
 	return arr;
-}
+	}
 
 
-void ShakerSort(int *a, int n,char** words)
-{
+void ShakerSort(int *a, int n, char** words)
+ {
 	int leftMark = 1;
 	int rightMark = n - 1;
 	char* tmp;
 	while (leftMark <= rightMark)
-	{
+		 {
 		for (int i = rightMark; i >= leftMark; i--)
-		if (a[i - 1] > a[i])
-		{
+			 if (a[i - 1] > a[i])
+			 {
 			Swap(a[i], a[i - 1]);
-			tmp = words[i];
-			words[i] = words[i - 1];
-			words[i - 1] = tmp;
-
-		}
+			Swap(words[i], words[i - 1]);
+			
+				}
 		leftMark++;
-
-
-		for (int i = leftMark; i <= rightMark; i++)
-		if (a[i - 1] > a[i])
-		{
+		
+			
+			for (int i = leftMark; i <= rightMark; i++)
+			 if (a[i - 1] > a[i])
+			 {
 			Swap(a[i], a[i - 1]);
-			tmp = words[i];
-			words[i] = words[i - 1];
-			words[i - 1] = tmp;
-		}
+			Swap(words[i], words[i - 1]);
+			}
 		rightMark--;
-
+		
+			}
 	}
-}
 
 
-void Display(int*k, char** str,int n)
-{
+void Display(int*k, char** str, int n)
+ {
 	
-	for (int i = 0; i < n;i++)
-	{
-		cout << str[i] << " - " << k[i] << " times"<<endl;
+		for (int i = 0; i < n; i++)
+		 {
+		cout << str[i] << " - " << k[i] << " times" << endl;
+		}
 	}
-}
 char** ObtainWords(char* str, int &k)
-{
-char* symbols = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
-	char** words = new char*[strlen(str)/2];
+ {
+	char* symbols = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
+	char** words = new char*[strlen(str) / 2];
 	k = 0;
-	char* pword = strpbrk(str,symbols);
-	while(pword)
-	{
-		int length = strspn(pword,symbols);
+	char* pword = strpbrk(str, symbols);
+	while (pword)
+		{
+		int length = strspn(pword, symbols);
 		words[k] = new char[length + 1];
-		strncpy(words[k],pword,length);
+		strncpy(words[k], pword, length);
 		words[k][length] = '\0';
 		pword += length;
-		pword = strpbrk(pword,symbols);
+		pword = strpbrk(pword, symbols);
 		k++;
-	}
+		}
 	return words;
-}     
+	}
 
 void Swap(int &x, int &y)
-{
+ {
 	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+	}
+
+void Swap(char* &x, char* &y)
+{
+	char* temp;
 	temp = x;
 	x = y;
 	y = temp;
